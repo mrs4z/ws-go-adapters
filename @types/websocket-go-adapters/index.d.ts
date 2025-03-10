b4d09911677c633c1149a7e8944ee5504447e691
mrs4z <mrs4z@icloud.com>
2025-03-11 00:41:43 +0300

    Update package configuration and type declarations
    - Add `typesVersions` to support subpath exports
    - Create type declarations for React and Vue adapters
    - Remove React Native export from main index file
    - Update package tarball

---
+++ @types/websocket-go-adapters/index.d.ts
@@ -0,0 +1,7 @@
+declare module 'websocket-go-adapters/react' {
+	export * from 'websocket-go-adapters/dist/types/react/index'
+}
+
+declare module 'websocket-go-adapters/vue' {
+	export * from 'websocket-go-adapters/dist/types/vue/index'
+}
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,3 @@ export * from './core/buffer-write'
 export * from './core/request'
 export * from './core/ws-core'
 export * from './types'
-
-// Export React Native adapter
-export * from './react-native/index.js'
